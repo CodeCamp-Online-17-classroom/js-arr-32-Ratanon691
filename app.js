4408c5165ac8778cee08c0c83fe5885a39d2b736
Non T <117605943+Ratanon691@users.noreply.github.com>
2024-07-07 09:57:46 +0700

    Update app.js

--- app.js
+++ app.js
@@ -1,1 +1,9 @@
-// write code here
+const array = [
+  { name: 'apple', age: 14 },
+  { name: 'banana', age: 18 },
+  { name: 'watermelon', age: 32 }
+];
+// result: ["apple", "banana", "watermelon"]
+let result = array.map(item => item.name)
+console.log(result)
+  
